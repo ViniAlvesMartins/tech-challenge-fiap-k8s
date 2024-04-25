3c1bae6f2dfd48aacff6a22d4dd919affdff1f47
MarcosBan <marcos.mps10@gmail.com>
2024-04-25 00:21:16 -0300

    Adicionando suporte a AWS academy

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
     required_version = ">= 1.2.0"
 
     backend "s3" {
-    bucket = "state-version-tech"
+    bucket = "ze-burger-tfstate"
     key = "terraform-eks-state"
     region = "us-east-1"
   }
@@ -19,9 +19,9 @@ provider "aws" {
 }
 
 data "aws_subnet" "eks_subnet_1" {
-  id = "subnet-0d54589568705e53e"
+  id = "subnet-0aec184325b04540f"
 }
 
 data "aws_subnet" "eks_subnet_2" {
-  id = "subnet-0ddcc9607d3c7aec5"
+  id = "subnet-09b5ff4b2628212c6"
 }
