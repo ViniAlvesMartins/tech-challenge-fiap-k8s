ea1253370805f749abbbcea62363613a6583fdc9
MarcosBan <marcos.mps10@gmail.com>
2024-05-01 19:33:17 -0300

    Fix output

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,10 +2,6 @@ output "cluster_name" {
   value = aws_eks_cluster.cluster.name
 }
 
-output "cluster_endpoint" {
-  value = aws_eks_cluster.cluster.endpoint
-}
-
 output "cluster_node_group" {
   value = aws_eks_node_group.ze-burguer-nodes.node_group_name
 }
